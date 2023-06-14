e4b7ad1c26358765c3363966f9d22c2eb1ae888e
IZC95 <131350379+IZC95@users.noreply.github.com>
2023-06-14 12:54:41 +0300

    Add files via upload

--- Routes/articles.js
+++ Routes/articles.js
@@ -1,18 +1,44 @@
 const express=require('express')
 const router=express.Router()
+const path=require('path')
 
-
-router.get("/contact",(req,res)=>{
-    res.render("views/contact")
-})
-router.get('/' , (req,res) => {
-    res.render("index", {
-
-    })
+router.get('/' , function(req,res)  {
+    res.sendFile(path.join(__dirname,"../Views/Home/index.html"))
 })
 
-// app.get('/contact/' , (req,res) => {
-//     res.render("contact", {
+router.get('/home' , function(req,res)  {
+    res.sendFile(path.join(__dirname,"../Views/Home/index.html"))
+})
 
-//     })
-// })
+router.get('/about' , function(req,res)  {
+    res.sendFile(path.join(__dirname,"../Views/Home/aboutUs.html"))
+})
+
+router.get('/products' , function(req,res)  {
+    res.sendFile(path.join(__dirname,"../Views/Product/allProducts.html"))
+})
+
+router.get('/contact' , function(req,res)  {
+    res.sendFile(path.join(__dirname,"../Views/Home/contact.html"))
+})
+
+router.get('/cart' , function(req,res)  {
+    res.sendFile(path.join(__dirname,"../Views/Cart/cart.html"))
+})
+
+router.get('/account' , function(req,res)  {
+    res.sendFile(path.join(__dirname,"../Views/User/account.html"))
+})
+
+router.get('/changepass' , function(req,res)  {
+    res.sendFile(path.join(__dirname,"../Views/User/changePass.html"))
+})
+
+router.get('/login' , function(req,res)  {
+    res.sendFile(path.join(__dirname,"../Views/User/Login.html"))
+})
+
+router.get('/signin' , function(req,res)  {
+    res.sendFile(path.join(__dirname,"../Views/User/Signin.html"))
+})
+module.exports=router
