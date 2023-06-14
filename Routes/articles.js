77cd74668f68816f938722c2f9dacee1a83e803d
IZC95 <131350379+IZC95@users.noreply.github.com>
2023-06-14 10:00:30 +0300

    Add files via upload

--- Routes/articles.js
+++ Routes/articles.js
@@ -1,1 +1,18 @@
 const express=require('express')
+const router=express.Router()
+
+
+router.get("/contact",(req,res)=>{
+    res.render("views/contact")
+})
+router.get('/' , (req,res) => {
+    res.render("index", {
+
+    })
+})
+
+// app.get('/contact/' , (req,res) => {
+//     res.render("contact", {
+
+//     })
+// })
