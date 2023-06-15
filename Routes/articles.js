6914d9d4e94ab81bf67ad4d259dbe0804e738b43
adirben98 <129677181+adirben98@users.noreply.github.com>
2023-06-15 16:19:08 +0300

    Add files via upload

--- Routes/articles.js
+++ Routes/articles.js
@@ -1,60 +1,25 @@
-const express=require('express')
-const router=express.Router()
+const express = require('express');
 const path=require('path')
-
-router.get('/' , function(req,res)  {
-    res.sendFile(path.join(__dirname,"../Views/Home/index.html"))
-})
-
-router.get('/home' , function(req,res)  {
-    res.sendFile(path.join(__dirname,"../Views/Home/index.html"))
-})
-
-router.get('/about' , function(req,res)  {
-    res.sendFile(path.join(__dirname,"../Views/Home/aboutUs.html"))
-})
-
-router.get('/products' , function(req,res)  {
-    res.sendFile(path.join(__dirname,"../Views/Product/allProducts.html"))
-})
-
-router.get('/contact' , function(req,res)  {
-    res.sendFile(path.join(__dirname,"../Views/Home/contact.html"))
-})
-router.get('/CSS' , function(req,res)  {
-    res.sendFile(path.join(__dirname,"../CSS/home.css"))
-})
-router.get('/Eggs1' , function(req,res)  {
-    res.sendFile(path.join(__dirname,"../Views/Product/eggs1.jpeg"))
-})
-router.get('/Eggs2' , function(req,res)  {
-    res.sendFile(path.join(__dirname,"../Views/Product/eggs2.jpeg"))
-})
-router.get('/Eggs3' , function(req,res)  {
-    res.sendFile(path.join(__dirname,"../Views/Product/eggs3.jpeg"))
-})
-router.get('/Eggs4' , function(req,res)  {
-    res.sendFile(path.join(__dirname,"../Views/Product/eggs4.jpeg"))
-})
+const router = express.Router();
 
 
-router.get('/cart' , function(req,res)  {
-    res.sendFile(path.join(__dirname,"../Views/Cart/cart.html"))
-})
+router.get('/home', function(req, res) {
+    res.sendFile(path.join(__dirname, 'contact.html'));
+  });
+  router.get('/signUp', function(req, res) {
+    res.sendFile(path.join(__dirname, 'Adir.html'))})
+  router.post('/search', function(req, res) {
+    const searchText = req.body.searchText;
+  
+    // Perform MongoDB query to find matching products
+    Product.find({ name: { $regex: searchText, $options: 'i' } }, function(err, products) {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'An error occurred' });
+      } else {
+        res.json(products);
+      }
+    });
+  });
 
-router.get('/account' , function(req,res)  {
-    res.sendFile(path.join(__dirname,"../Views/User/account.html"))
-})
-
-router.get('/changepass' , function(req,res)  {
-    res.sendFile(path.join(__dirname,"../Views/User/changePass.html"))
-})
-
-router.get('/login' , function(req,res)  {
-    res.sendFile(path.join(__dirname,"../Views/User/Login.html"))
-})
-
-router.get('/signin' , function(req,res)  {
-    res.sendFile(path.join(__dirname,"../Views/User/Signin.html"))
-})
-module.exports=router
+module.exports = router;
