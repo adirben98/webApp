1850bcfe94ff444cb23cf3479a1e79eedf63c5ef
adirben98 <129677181+adirben98@users.noreply.github.com>
2023-06-15 10:52:37 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ const app=express()
 const bodyParser=require('body-parser')
 const cors=require('cors')
 const mongoose=require('mongoose')
+const expressSession=require('express-session')
 
-mongoose.connect('mongodb://localhost:27017/store',
-{useNewUrlParser:true,
-useUnifyTopology:true})
+
+mongoose.connect('mongodb://127.0.0.1:27017/local?directConnection=true', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 app.use(cors())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.json())
 app.use('/',require('./Routes/articles'))
-app.set("view engine","ejs")
+app.use(expressSession({ secret: 'mySecretKey', resave: true, saveUninitialized: true }));
 
+app.set('view engine','ejs')
 
 app.listen(8080)
 
