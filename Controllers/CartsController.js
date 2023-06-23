364a832d10cf07b04b1eb8a5b580fef0a5d72039
adirben98 <129677181+adirben98@users.noreply.github.com>
2023-06-23 11:33:51 +0300

    Add files via upload

--- Controllers/CartsController.js
+++ Controllers/CartsController.js
@@ -1,49 +1,19 @@
 const articleService = require('../Services/CartService');
-
-const createArticle = async (req,res) => {
-  const newArticle = await articleService.createArticle(req.body.title);
-  res.json(newArticle);
-}
-
-
-const getArticles = async (req,res) => {
-  const articles = await articleService.getArticles();
-  res.json(articles);
-}
-
-const getArticleById = async (req,res) => {
-  const article = await articleService.getArticleById(req.params.id);
-  if (!article){
-    return res.status(404).json({errors:['Article not found']});
+ let cart=[]
+const addToCart= async (req,res) =>{
+  cart.forEach(function(cartItem){
+    if (cartItem.name === req.body.name) {
+      
+      cartItem.quantity++;
+      productExists = true;
+      return;
   }
-  res.json(article);
+  })
 }
+const checkOut= async (req,res) =>{}
 
-const updateArticle = async (req,res) => {
-  if (!req.body.title){
-    res.status(400).json({message:'Title is required'});
-  }
-
-  const article = await articleService.updateArticle(req.body.id,req.body.title);
-  if (!article){
-    return res.status(404).json({errors:['Article not found']});
-  }
-  res.json(article);
-};
-
-
-const deleteArticle = async (req,res) => {
-  const article = await articleService.deleteArticle(req.params.id);
-  if (!article){
-    return res.status(404).json({errors:['Article not found']});
-  }
-  res.send();
-}
 
 module.exports = {
-    createArticle,
-    getArticles,
-    getArticleById,
-    updateArticle,
-    deleteArticle
+   addToCart,
+   checkOut
 }
