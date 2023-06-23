e3c2f68b0090fa1a0384c48f32bec86f49789d9a
adirben98 <129677181+adirben98@users.noreply.github.com>
2023-06-23 11:27:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ mongoose.connect('mongodb+srv://adirben:shayhorovitz@shop.tv7a2dc.mongodb.net/',
 app.use(cors())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.json())
-app.use('/',require('./Routes/articles'))
 app.use(Session({ secret: 'mySecretKey', resave: true, saveUninitialized: true }));
 
 app.set('view engine','ejs')
+app.use('/',require('./Routes/articles'))
 
 app.listen(8080)
 
