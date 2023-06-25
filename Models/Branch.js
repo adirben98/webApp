5d4e7b6e6e247c59a9c5ab0c359b341156fd8a5c
IZC95 <131350379+IZC95@users.noreply.github.com>
2023-06-25 18:04:00 +0300

    Update Branch.js

--- Models/Branch.js
+++ Models/Branch.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const branch = new Schema({
-
+    _id:{
+        type: String,
+        required:true
+    },
     name: {
         type: String,
         required:true
