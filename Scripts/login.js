ffb2ebc77849a4209dec615c7ef0b85ab6e84f11
IZC95 <131350379+IZC95@users.noreply.github.com>
2023-06-25 13:22:27 +0300

    Update login.js

--- Scripts/login.js
+++ Scripts/login.js
@@ -1,12 +1,13 @@
 const loginService=require('../Services/loginService')
+const UserService=require('../Services/UserService')
 
 async function login(req, res) {
   const { email, password } = req.body
-  
+  const User= await UserService.getUser(email)
   const result = await loginService.login(email, password)
   if (result) {
     req.session.email = email
-   
+   req.session.firstName = User.firstName;
 
     res.redirect('/')
   }
@@ -27,6 +28,7 @@ async function login(req, res) {
         console.log(password)
         if (request) {
           req.session.email = email;
+          req.session.firstName = firstName;
           return res.redirect('/');
         } else {
        
@@ -50,10 +52,19 @@ async function login(req, res) {
       loginService.changePass(email,newPassword)
 
   }
+function isloggedin(req,res){
+    if(req.session.email)
+    res.json({isloggedin:true,
+              email:req.session.email,
+            firstName:req.session.firstName})
+    else
+    res.json({isloggedin:false})
+  }
   
   module.exports = {
     login,
     register,
     logout,
-    changePass
+    changePass,
+    isloggedin
 }
