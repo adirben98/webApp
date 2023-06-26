d84bfeceaa4dade97ea75d120465e941183ce235
adirben98 <129677181+adirben98@users.noreply.github.com>
2023-06-26 20:28:42 +0300

    Add files via upload

--- Scripts/index.js
+++ Scripts/index.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
     success: function (html) {
       $("#header-placeholder").html(html);
       $("#search-input").on("input", searchProducts);
+      $('input[type="checkbox"]').on('change', applyFilters);
 
       $.ajax({
         url: "/footer.html",
@@ -79,3 +80,71 @@ $(document).ready(function () {
     }
   }
 });
+function applyFilters() {
+  // Get the selected filter options
+  var selectedSize = [];
+  var selectedBreed = [];
+  var selectedTraySize = [];
+
+  $('input[type="checkbox"]').each(function() {
+    if (this.checked) {
+      var checkboxValue = $(this).attr('value');
+      var checkboxId = $(this).attr('id');
+
+      if (checkboxId.startsWith('Small') || checkboxId.startsWith('Medium') || checkboxId.startsWith('Large')) {
+        selectedSize.push(checkboxValue);
+      } else if (checkboxId.startsWith('breed')) {
+        selectedBreed.push(checkboxValue);
+      } else if (checkboxId.startsWith('trayOf') || checkboxId === 'singles') {
+        selectedTraySize.push(checkboxValue);
+      }
+    }
+  });
+
+  // Create the filter object
+  var filters = {
+    eggSize: selectedSize,
+    category: selectedBreed,
+    traySize: selectedTraySize
+  };
+
+  // Send the filter data to the server
+  $.ajax({
+    url: '/filters',
+    method: 'POST',
+    data: filters,
+    dataType: 'json',
+    success: function(response) {
+      // Handle the response from the server
+      var products = response.products;
+
+      // Update the product listing based on the filtered results
+      var productContainer = $('#product-list');
+      productContainer.empty(); // Clear previous products
+
+      if (products && products.length > 0) {
+        products.forEach(function(Product) {
+          var productHtml = `
+            <div class="egg-card">
+              <h2>${Product.name}</h2>
+              <img src="${Product.image}" alt="${Product.name}">
+              <p>${Product.category} eggs</p>
+              <p>Size: ${Product.EggSize}</p>
+              <p>Tray of-${Product.traysize}</p>
+              <p>Price: $${Product.price}</p>
+              <a href="/products/${Product.name}" class="btn btn-info">View Product</a>
+            </div>
+          `;
+          $('#product-list').append(productHtml);
+
+        });
+      } else {
+        var noResultsElement = $('<div>').text('No products found.');
+        productContainer.append(noResultsElement);
+      }
+    },
+    error: function(xhr, status, error) {
+      console.error('Error applying filters:', error);
+    }
+  });
+}
