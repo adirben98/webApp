cc317f740267957f5e29ab2e373cc3bb79f9aba0
adirben98 <129677181+adirben98@users.noreply.github.com>
2023-06-28 11:51:59 +0300

    Update ProductsController.js

--- Controllers/ProductsController.js
+++ Controllers/ProductsController.js
@@ -1,5 +1,5 @@
 const ProductService = require('../Services/ProductService');
-const axios=require('axios')
+
 
 const createProduct = async (req,res) => {
   const {
@@ -75,39 +75,7 @@ const filter=async(req,res)=>{
 
 }
 
-const facebook = async (req, res) => {
-  try {
-    const accessToken = 'EAASQcjtNN2IBAGl81pHGZBCVRWtF5tlQqNYAMAZBhl7xxasi6sktQoKCZAYlUIGwN1PV2ZBIq5P5iCzeZCihaXOZAafhtZBp4o5qudibZB271qRKvKGVQqJ5evHY15Uaa9msoCPZAjPpkSJ5k1uEGFKj5ijswtRYZCg8SQqK32fJThpiEuRbVIisKnm9kWTlNxl6edCNlspAsiWmGq52fDTxlo'; // Access token obtained from Facebook Login
-    const message = req.body.name; // The message for the post
-    const imageUrl = req.body.image; // URL of an image to include in the post
 
-    // Make a POST request to the Facebook API
-    const response = await axios.post(
-      `https://graph.facebook.com/me/feed`,
-      {
-        message,
-        attached_media: [
-          {
-            media_type: 'IMAGE',
-            media_url: imageUrl,
-          },
-        ],
-      },
-      {
-        params: {
-          access_token: accessToken,
-        },
-      }
-    );
-
-    // Handle the response from the Facebook API
-    console.log('Post created on Facebook:', response.data);
-    res.status(200).json({ success: true });
-  } catch (error) {
-    console.error('Error creating post on Facebook:', error.response.data);
-    res.status(500).json({ success: false, error: error.message });
-  }
-};
 
 
 module.exports = {
@@ -119,6 +87,6 @@ module.exports = {
     deleteProduct,
     search,
     filter,
-    facebook
+    
 
 }
