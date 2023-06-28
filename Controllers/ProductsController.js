bd0d0e2dc071c4712b7e827bf3f56bd1268a701b
adirben98 <129677181+adirben98@users.noreply.github.com>
2023-06-28 14:25:57 +0300

    Update ProductsController.js

--- Controllers/ProductsController.js
+++ Controllers/ProductsController.js
@@ -32,9 +32,6 @@ const getProductById=async (req,res) => {
 
 
 const updateProduct = async (req,res) => {
-  if (!req.body.title){
-    res.status(400).json({message:'title is required'});
-  }
 
   const product = await ProductService.updateProduct(req.body.productid,req.body.name,req.body.category,req.body.size,req.body.traysize,req.body.price,req.body.description,req.body.supplier);
   if (!product){
@@ -45,11 +42,11 @@ const updateProduct = async (req,res) => {
 
 
 const deleteProduct = async (req,res) => {
-  const product = await ProductService.deleteProduct(req.params.id);
+  const product = await ProductService.deleteProduct(req.body.name);
   if (!product){
     return res.status(404).json({errors:['Product not found']});
   }
-  res.send();
+  
 }
 const search=async(req,res)=>{
   const searchResult= await ProductService.search(req.query.query)
@@ -74,10 +71,6 @@ const filter=async(req,res)=>{
     });
 
 }
-
-
-
-
 module.exports = {
     createProduct,
     getProduct,
@@ -87,6 +80,4 @@ module.exports = {
     deleteProduct,
     search,
     filter,
-    
-
 }
