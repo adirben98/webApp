43e0b960c5bbc851c838d130877e6ce72a2f4e55
talgins47 <131457400+talgins47@users.noreply.github.com>
2023-07-03 10:56:21 +0300

    Update UsersController.js

--- Controllers/UsersController.js
+++ Controllers/UsersController.js
@@ -3,6 +3,14 @@ const UserService = require('../Services/UserService');
 const createUser = async (req,res) => {
     const newUser = await UserService.createUser(req.body.firstName,req.body.lastName,req.body.email,req.body.password);
     res.json(newUser);
+
+}
+
+const createAdmin = async (req,res) => {
+    const newAdmin = await UserService.createAdmin(req.body.firstName,req.body.lastName,req.body.email,req.body.password,"admin");
+    if(newAdmin)
+    return res.redirect('/admin')
+    else return res.redirect('/admin/createUser?error=1')
 }
 
 const getUser = async (req,res) => {
@@ -19,26 +27,27 @@ const getUsers = async (req,res) => {
 }
 
 const updateUser = async (req,res) => {
-    const User = await UserService.updateUser(req.body.firstName,req.body.lastName,req.body.email,req.body.password);
-    if (!User){
-      return res.status(404).json({errors:['User not found']});
+    const User = await UserService.updateUser(req.body.existingEmail,req.body.firstName,req.body.lastName,req.body.userType);
+    if (!User)
+    return  res.redirect('/admin/updateUser?error=1')
+    else return res.redirect('/admin')
     }
-    res.json(User);
-}
 
 const deleteUser = async (req,res) =>{
     const User = await UserService.deleteUser(req.body.email);
-    if (!User){
-      return res.status(404).json({errors:['User not found']});
+    if(User)
+    return res.redirect('/admin')
+    else return res.redirect('/admin/deleteUser?error=1');
     }
     
-}
+
 
 module.exports = {
     createUser,
     getUser,
     getUsers,
     updateUser,
-    deleteUser
+    deleteUser,
+    createAdmin
 }
 
